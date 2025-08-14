216d8ee62415a64af756550d4816dba1598315b3
Alexandre Dantas dos Santos <allexsantosrn@gmail.com>
2025-08-13 23:41:40 -0300

    initialize project with Spring Boot and Maven setup

---
+++ encurtador-links/src/main/java/com/projeto/encurtador_links/EncurtadorLinksApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.encurtador_links;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtadorLinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtadorLinksApplication.class, args);
+	}
+
+}
---
+++ encurtador-links/src/test/java/com/projeto/encurtador_links/EncurtadorLinksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.encurtador_links;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadorLinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
